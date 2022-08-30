20f596b965d8fe6eda917d5b850ddf82ac7d720c
Camyla Castro <camyla.clima@gmail.com>
2022-08-29 21:58:52 -0300

    Incluindo mensagem

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,11 @@ let email = "camyla.clima@gmail.com"
 let profissao = "Assistente Administrativo"
 let idade = "32 anos"
 let lugar = "Suécia"
+let mensagem = "Olá, "  + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para "  + lugar + ". Confirma pra gente alguns dados? A sua idade é " + idade + ", você é " + profissao + " e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugar)
+console.log(mensagem)
